2e17efc5b5a84dabffd69bbc55a4bc019104646e
Egor Gurba <teninworkprime@gmail.com>
2020-02-11 18:53:42 +0200

    Initial Commit

---
+++ TableShulte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableShulte
+//
+//  Created by Егор on 11.02.2020.
+//  Copyright © 2020 Egor Gurba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
